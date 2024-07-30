eb1ccb664709713cd467618477d7645de9c7e735
Ng Quyen <quyen10924@gmail.com>
2024-07-30 23:26:43 +0700

    sua lai footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,6 +5,7 @@ import { Container, Row, Col } from 'react-bootstrap';
 import {FaMapMarkerAlt, FaPhone, FaEnvelope } from 'react-icons/fa';
 import { FaFacebookF } from 'react-icons/fa';
 import { SiTiktok } from 'react-icons/si';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const listAbout = [
     {
@@ -28,25 +29,77 @@ const listAbout = [
         title: "koreankuisine@gmail.com",
         path: "/mail"
     }
+]
 
+const listLink = [
+  {
+    id:1,
+    title:"Về chúng tôi",
+    path:"/"
+  },
+  {
+    id:2,
+    title:"Thực đơn",
+    path:"/"
+  },
+  {
+    id:3,
+    title:"Đặt bàn",
+    path:"/"
+  },
+  {
+    id:4,
+    title:"Tuyển dụng",
+    path:"/"
+  },
+  {
+    id:5,
+    title:"Tin tức",
+    path:"/"
+  }
+]
+
+const listFanpage = [
+  {
+    id:1,
+    icon: <FaFacebookF />,
+    path:""
+  },
+  {
+    id:2,
+    icon: <SiTiktok />,
+    path:""
+  },
+  {
+    id:3,
+    icon: <FaFacebookF />,
+    path:""
+  }
 ]
 
 const Footer = () => {
     return (
       <Container fluid className="footer-container">
         <Row>
-          <Col xs={12} md={4} className="footer-section">
-            <div className="footer-logo">
+          <Col className="footer-section d-flex justify-content-center">
+            <Col xs={2} md={2}  className="footer-logo">
               <img src={Logo} alt='Logo' className='logo' />
-              <h1 className='res-title res-name'>KOREAN-KUISINE</h1>
-            </div>
-            <p>
-              Với mong muốn luôn luôn lắng nghe và phát triển, chúng tôi rất vui và lấy làm vinh dự khi được bạn góp ý – đánh giá về 
-              dịch vụ của mình. Chúng tôi sẽ tiếp thu và phát triển dịch vụ hoàn thiện hơn từng ngày.
-            </p>
+              <h1 className='res-name'>KOREAN KUISINE</h1>
+            </Col>
+            <Col xs={2} md={2}>
+              <div className="vertical-hr"></div>
+            </Col>
+            <Col xs={8} md={8} className='footer-slogan'>
+              <p className='text-slogan'>
+                Với mong muốn luôn luôn lắng nghe và phát triển, chúng tôi rất vui và lấy làm vinh dự khi được bạn góp ý – đánh giá về 
+                dịch vụ của mình. Chúng tôi sẽ tiếp thu và phát triển dịch vụ hoàn thiện hơn từng ngày.
+              </p>
+            </Col>
           </Col>
+        </Row>
+        <Row>
           <Col xs={12} md={4} className="footer-section">
-            <h3 className='res-title'>Về chúng tôi</h3>
+            <h3 className='res-title'>Liên Hệ</h3>
             <ul className='list list-about'>
               {listAbout.map((about) => (
                 <li key={about.id} className='about-items'>
@@ -57,12 +110,29 @@ const Footer = () => {
               ))}
             </ul>
           </Col>
-          <Col xs={12} md={4} className="footer-section">
-            <h3 className='res-title'>Liên Hệ</h3>
-            <div className='list-contact'>
-              <button className='facebook'> <FaFacebookF /></button>
-              <button className='tiktok'> <SiTiktok /></button>
-            </div>
+          <Col xs={6} md={4} className="footer-section">
+            <h3 className='res-title'>Liên kết</h3>
+            <ul className='list list-link'>
+              {listLink.map((link) => (
+                <li key={link.id} className='about-items'>
+                  <a href={link.path} className='about-title'>
+                    {link.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </Col>
+          <Col xs={6} md={4} className="footer-section">
+            <h3 className='res-title'>Fanpage</h3>
+            <ul className='list list-link'>
+              {listFanpage.map((fanpage) => (
+                <li key={fanpage.id} className='about-items'>
+                  <a href={fanpage.path} className='about-title'>
+                    {fanpage.icon}
+                  </a>
+                </li>
+              ))}
+            </ul>
           </Col>
         </Row>
         <Row>
