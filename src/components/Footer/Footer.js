00fcb3202d96b9cf1f9e76d3d448ed5d1f39685a
Ng Quyen <quyen10924@gmail.com>
2024-07-30 18:27:41 +0700

    bo xung footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import './Footer.css';
 import Logo from '../../assets/food/logo.png'
 import {motion} from "framer-motion";
@@ -7,17 +6,17 @@ import { Container, Col, Row } from 'react-bootstrap';
 const listAbout = [
     {
         id:1,
-        title:"abc, bcd, HCM, VIETNAM",
+        title:"Location: abc, bcd, HCM, VIETNAM",
         path: "/location"
     },
     {
         id:2,
-        title: "0373730397",
+        title: "Hotline: 0373730397",
         path: "/phone"
     },
     {
         id:3,
-        title: "example@example.com",
+        title: "Email: example@example.com",
         path: "/mail"
     }
 
@@ -26,32 +25,56 @@ const listAbout = [
 const footer = () => {
     return (
     <Container className='container-footer'>
-      <Row>
-        {/* Logo + TenNhaHang + DiaChi + sdt + mail */}
-        <Col>
-            <motion.div>
-                <motion.img src={Logo} alt='Logo' className='logo' />
-
-            </motion.div>
-            <h1 className='res-name'>KOREAN-KUISINE</h1>
-            <div className='About'>
-                <ul className='list-about'>
-                    {listAbout.map((about) =>{
-                        return (
-                            <motion.li
-                             key={about.id} className='about-items'>
-                                <a href={about.path} className='about-title'>{about.title}</a>
-                            </motion.li>
-                        );
-                    })}
-                </ul>
-            </div>
-        </Col>
-        {/*  */}
-        <Col>2 of 3</Col>
-        {/* Lien he + mang xa hoi */}
-        <Col>3 of 3</Col>
-      </Row>
+        <Row>
+            <Col xs={8} md={12}>
+                <motion.div>
+                    <motion.img src={Logo} alt='Logo' className='logo' />
+                    <h1 className='res-title res-name'>KOREAN-KUISINE</h1>
+                </motion.div> 
+                <p>
+                Với mong muốn luôn luôn lắng nghe và phát triển, 
+                chúng tôi rất vui và lấy làm vinh dự khi được bạn góp ý – đánh giá về 
+                dịch vụ của mình.
+                Chúng tôi sẽ tiếp thu và phát triển dịch vụ hoàn thiện hơn từng ngày.
+                </p>
+            </Col>
+        </Row>
+        <Row>
+            {/* Logo + TenNhaHang + DiaChi + sdt + mail */}
+            <Col xs={6} md={4}>
+                <div className='About'>
+                    <ul className='list-about'>
+                        {listAbout.map((about) =>{
+                            return (
+                                <motion.li
+                                key={about.id} className='about-items'>
+                                    <a href={about.path} className='about-title'>{about.title}</a>
+                                </motion.li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            </Col>
+            {/*  */}
+            <Col xs={6} md={4}> 
+                <h1 className='res-title res-about'>Về chúng tôi</h1>
+            </Col>
+            {/* Lien he + mang xa hoi */}
+            <Col xs={6} md={4}>
+                <h1 className='res-title res-contact'>Liên Hệ</h1>
+                <div className='list-contact'>
+                    <button className='list-contact-facebook' href=""></button>
+                    <button className='list-contact-zalo' href=""></button>
+                    <button className='list-contact-tiktok' href=""></button>
+                </div>
+            </Col>
+        </Row>
+        <Row>
+            <Col><hr></hr></Col>
+        </Row>
+        <Row>
+            <h5 className='text-coppyright'>© Copyright 2024 by Korean-Kuisine restaurant. All Rights Reserved.</h5>
+        </Row>
     </Container>
 )};
 
