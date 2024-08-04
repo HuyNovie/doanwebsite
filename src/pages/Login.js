b86a22d0a440556320be7b07f4460737ac1382af
Quyen Ngoc <quyen10924@gmail.com>
2024-08-04 18:27:25 +0700

    xong tinh nang login va dang ky

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,47 +1,44 @@
 import React, { useState } from "react";
-import "./Login.css"
-import {Button,Input} from 'antd';
+import "./Login.css";
+import { Button, Input } from "antd";
 import { Link } from "react-router-dom";
-import { FaUser, FaEyeSlash, FaEye  } from "react-icons/fa";
+import { FaUser, FaEyeSlash, FaEye } from "react-icons/fa";
 
 const Login = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [mail, setMail] = useState("");
-    const [phone, setPhone] = useState("");
-    const [firstName, setFisrtName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [dayOfBirth, setDayOfBirth] = useState("");
-    let [errorMessage, setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const [user,setUser] = useState({
-    email: "",
+  const [user, setUser] = useState({
+    username: "",
     password: "",
-});
+  });
 
-  // kiem tra value co hay khong
-  const [emailError, setEmailError] = useState("");
+  const [usernameError, setUsernameError] = useState("");
   const [passwordError, setPasswordError] = useState("");
 
   const validateData = (name, value) => {
     let isValid = true;
-    switch(name) {
-      case 'email':
+    switch (name) {
+      case "username":
         if (!value) {
-          setEmailError("Vui lòng điền vào mục này.");
+          setUsernameError("Vui lòng điền vào mục này.");
+          isValid = false;
+        } else if (value.length < 8) {
+          setUsernameError("Tên tài khoản phải dài hơn 8 kí tự");
           isValid = false;
         } else {
-                setEmailError("")  
-            }
-        
+          setUsernameError("");
+        }
         break;
-      case 'password':
+      case "password":
         if (!value) {
           setPasswordError("Vui lòng điền vào mục này.");
           isValid = false;
-          }else {
+        } else if (value.length < 8) {
+          setPasswordError("Mật khẩu phải dài hơn 8 kí tự");
+          isValid = false;
+        } else {
           setPasswordError("");
-            }
+        }
         break;
       default:
         break;
@@ -49,57 +46,58 @@ const Login = () => {
     return isValid;
   };
 
-  // lay gia tri tu input
-  const handeChange = (e) => {
-      const {value, name} = e.target;
+  const handleChange = (e) => {
+    const { value, name } = e.target;
 
-      setUser({
-          ...user,
-          [name]:value,
-      });
-      validateData(name, value);
+    setUser({
+      ...user,
+      [name]: value,
+    });
+    validateData(name, value);
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  const handleSubmit = (e) => {
-      e.preventDefault();
+    const usernameValid = validateData("username", user.username);
+    const passwordValid = validateData("password", user.password);
 
-      const emailValid = validateData("email", user.email);
-      const passwordValid= validateData("password", user.password);
+    if (usernameValid && passwordValid) {
+      try {
+        const response = await fetch(
+          "http://localhost:8080/identity/auth/login",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              username: user.username,
+              password: user.password,
+            }),
+          }
+        );
 
-  }
-  //   // handle cho form login
-//   const handleSubmitLogin = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const response = await fetch(
-//         "http://localhost:8080/identity/auth/login",
-//         {
-//           method: "POST",
-//           headers: {
-//             "Content-Type": "application/json",
-//           },
-//           body: JSON.stringify({ username, password }),
-//         }
-//       );
+        const data = await response.json();
+        if (data.result && data.result.authenticated) {
+          console.log("Login successful:", data.result);
 
-//       const data = await response.json();
-//       if (data.result.authenticated) {
-//         console.log("Login successful:", data.result);
+          localStorage.setItem("token", data.result.token);
+          window.location.href = "/";
+        }
+        if (data.code === 1005) {
+          setErrorMessage("Tài khoản không tồn tại");
+        }
+        if (data.code === 1006) {
+          setErrorMessage("Mật khẩu không chính xác!");
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        setErrorMessage("Vui lòng thử lại sau");
+      }
+    }
+  };
 
-//         localStorage.setItem("token", data.result.token);
-//         window.location.href = "/";
-//       } else {
-//         setErrorMessage = data.message;
-//       }
-//     } catch (error) {
-//       console.error("Error:", error);
-//       setErrorMessage("Please try again.");
-//     }
-//   };
-
-
-  //show password
   const [passwordShown, setPasswordShown] = useState(false);
 
   const togglePasswordVisibility = () => {
@@ -107,51 +105,62 @@ const Login = () => {
   };
 
   return (
-  <div className='wrapper'>
+    <div className="wrapper">
       <form onSubmit={handleSubmit}>
-          <h1>Login</h1>
-          <div className='form-group'>
-              <Input type='email' onChange={handeChange} name='email' status = {emailError? 'error' : ''} placeholder='Email' />
-              <span className='items-form'><FaUser /></span>
-              {emailError &&(
-              <p className='error-message'>{emailError}</p>
-          )}
-          
-          </div>
+        <h1>Login</h1>
+        <div className="form-group">
+          <Input
+            type="text"
+            onChange={handleChange}
+            name="username"
+            status={usernameError ? "error" : ""}
+            placeholder="Tên tài khoản"
+          />
+          <span className="items-form">
+            <FaUser />
+          </span>
+          {usernameError && <p className="error-message">{usernameError}</p>}
+        </div>
 
-          <div className='form-group'>
-              <Input type={passwordShown ? "text" : "password"} onChange={handeChange} status = {passwordError ? 'error' : ''} name='password' placeholder='Mật khuẩu' />
-              <span onClick={togglePasswordVisibility} className='items-form'>
-                  {passwordShown ? <FaEyeSlash /> : <FaEye />}
-              </span>
-              {passwordError &&(
-              <p className='error-message'>{passwordError}</p>
-          )}
-          </div>
+        <div className="form-group">
+          <Input
+            type={passwordShown ? "text" : "password"}
+            onChange={handleChange}
+            status={passwordError ? "error" : ""}
+            name="password"
+            placeholder="Mật khẩu"
+          />
+          <span onClick={togglePasswordVisibility} className="items-form">
+            {passwordShown ? <FaEyeSlash /> : <FaEye />}
+          </span>
+          {passwordError && <p className="error-message">{passwordError}</p>}
+        </div>
 
-          <div className='remember-forgot'>
-              <label><input className='checkbox' type='checkbox'/>Ghi nhớ</label>
-              <Link to = '/'>Quên mật khẩu?</Link>      
-          </div>
-          <div >
-              <Button htmlType='submit' type='primary'>
-                  Đăng nhập
-              </Button>
-          </div> 
-          
-          <div className='register-link'>
-              <p>Bạn chưa có tài khoản ?
-              <Link to= "/register" className='text-register'>Đăng kí</Link> 
-              </p>
-          </div>
+        <div className="remember-forgot">
+          <label>
+            <input className="checkbox" type="checkbox" />
+            Ghi nhớ
+          </label>
+          <Link to="/">Quên mật khẩu?</Link>
+        </div>
+        <div>
+          <Button htmlType="submit" type="primary">
+            Đăng nhập
+          </Button>
+        </div>
+
+        <div className="register-link">
+          <p>
+            Bạn chưa có tài khoản ?
+            <Link to="/register" className="text-register">
+              Đăng kí
+            </Link>
+          </p>
+        </div>
+        {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
       </form>
-  </div>
-    );
-  }
-
-
-
-
-
+    </div>
+  );
+};
 
 export default Login;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,256 +1,213 @@
-
-
-import React from 'react'
-import './Register.css'
-import {Button,Input} from 'antd';
+import React from "react";
+import "./Register.css";
+import { Button, Input } from "antd";
 import { Link } from "react-router-dom";
-import { useState } from 'react';
-import validateEmail from '../utils/validateData';
+import { useState } from "react";
+import validateEmail from "../utils/validateData";
 import { FaEyeSlash, FaEye } from "react-icons/fa";
 
-
 const Register = () => {
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [mail, setMail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [firstName, setFisrtName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [dayOfBirth, setDayOfBirth] = useState("");
-  let [errorMessage, setErrorMessage] = useState("");
-  
-    const [user, setUser] = useState({
-        fullName: '',
-        email: '',
-        password: '',
-        status: 1,
-      });
-     // kiem tra value co hay khong
-      const [fullNameError, setFullNameError] = useState("");
-      const [emailError, setEmailError] = useState("");
-      const [passwordError, setPasswordError] = useState("");
+  const [user, setUser] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+    status: 1,
+  });
 
-      const validateData = (name, value) => {
-        let isValid = true;
-        switch(name) {
-          case 'fullName':
-            if (!value) {
-              setFullNameError("Vui lòng điền vào mục này.");
-              isValid = false;
-            } else {
-              setFullNameError("");
-            }
-            break;
-          case 'email':
-            if (!value) {
-              setEmailError("Vui lòng điền vào mục này.");
-              isValid = false;
-            } else {
-                if(!validateEmail(value)){
-                  setEmailError("Email không được định dạng đúng!");
-                  isValid = false;  
-                }else{
-                    setEmailError("")  
-                }
-            }
-            break;
-          case 'password':
-            if (!value) {
-              setPasswordError("Vui lòng điền vào mục này.");
-              isValid = false;
-              } else {
-                const errors = [];
-                if (value.length < 5) errors.push("ít nhất 6 ký tự");
-                if (!/[a-z]/.test(value)) errors.push("chữ thường");
-                if (!/[A-Z]/.test(value)) errors.push("chữ hoa");
-                if (!/\d/.test(value)) errors.push("ký tự số");
-                if (!/[^A-Za-z0-9]/.test(value)) errors.push("ký tự đặc biệt");
-                if (errors.length > 0) {
-                  setPasswordError(`Cần ${errors.join(', ')}`);
-                  isValid = false;
-                } else {
-                  setPasswordError("");
-                }
-            }
-            break;
-          default:
-            break;
+  const [fullNameError, setFullNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const validateData = (name, value) => {
+    let isValid = true;
+    switch (name) {
+      case "fullName":
+        if (!value) {
+          setFullNameError("Vui lòng điền vào mục này.");
+          isValid = false;
+        } else {
+          setFullNameError("");
         }
-        return isValid;
-      };
-        //
-      const handleChange = (e) => {
-        const { name, value } = e.target;
-        setUser({
-          ...user,
-          [name]: value,
-        });
-        validateData(name, value);
-      }; 
-    
-      const handleSubmit = (e) => {
-       e.preventDefault();
-       
-        const fullNameValid = validateData("fullName", user.fullName);
-        const emailValid = validateData("email", user.email);
-        const passwordValid= validateData("password", user.password);
-        
-        
-        // if (fullNameValid && emailValid && passwordValid){
-        //   // Goi API dang ki
-         
-        // }
+        break;
+      case "email":
+        if (!value) {
+          setEmailError("Vui lòng điền vào mục này.");
+          isValid = false;
+        } else {
+          if (!validateEmail(value)) {
+            setEmailError("Email không được định dạng đúng!");
+            isValid = false;
+          } else {
+            setEmailError("");
+          }
+        }
+        break;
+      case "password":
+        if (!value) {
+          setPasswordError("Vui lòng điền vào mục này.");
+          isValid = false;
+        } else {
+          const errors = [];
+          if (value.length < 8) errors.push("Mật khẩu phải ít nhất 8 ký tự");
+          if (errors.length > 0) {
+            setPasswordError(`Cần ${errors.join(", ")}`);
+            isValid = false;
+          } else {
+            setPasswordError("");
+          }
+        }
+        break;
+      default:
+        break;
+    }
+    return isValid;
+  };
 
-      };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+    validateData(name, value);
+  };
 
-      //   //   handle cho form register
-//   const handleSubmitRegister = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const response = await fetch(
-//         "http://localhost:8080/identity/users/register",
-//         {
-//           method: "POST",
-//           headers: {
-//             "Content-Type": "application/json",
-//           },
-//           body: JSON.stringify({
-//             username,
-//             password,
-//             mail,
-//             phone,
-//             firstName,
-//             lastName,
-//             dayOfBirth,
-//           }),
-//         }
-//       );
+  const unorm = require("unorm");
 
-//       const data = await response.json();
-//       if (data.code === 1000) {
-//         console.log("Register successful:", data.result);
-
-//         window.location.href = "/";
-//       }
-//       if (data.code === 1002) {
-//         console.log("Register fail:", data.message);
-
-//         setErrorMessage("Tài khoản đã tồn tại");
-//       }
-//     } catch (error) {
-//       console.error("Error:", error);
-//       setErrorMessage("Vui lòng thử lại sau.");
-//     }
-//   };
-
-
-
-
-       //show password
-    const [passwordShown, setPasswordShown] = useState(false);
-
-    const togglePasswordVisibility = () => {
-      setPasswordShown(!passwordShown);
+  function generateUsername(fullName) {
+    const removeAccents = (str) => {
+      return unorm.nfd(str).replace(/[\u0300-\u036f]/g, "");
     };
 
+    const cleanedName = removeAccents(fullName)
+      .replace(/\s+/g, "")
+      .toLowerCase();
+    const randomDigits = Math.floor(1000 + Math.random() * 9000);
+    return `${cleanedName}${randomDigits}`;
+  }
 
-  //form dang ki    
+  function generateFirstName(fullName) {
+    const nameParts = fullName.split(" ");
+    return nameParts.slice(-1).join(" ");
+  }
+
+  function generateLastName(fullName) {
+    const nameParts = fullName.split(" ");
+    return nameParts.slice(0, -1).join(" ");
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const fullNameValid = validateData("fullName", user.fullName);
+    const emailValid = validateData("email", user.email);
+    const passwordValid = validateData("password", user.password);
+
+    if (fullNameValid && emailValid && passwordValid) {
+      const username = generateUsername(user.fullName);
+      const firstName = generateFirstName(user.fullName);
+      const lastName = generateLastName(user.fullName);
+      const dayOfBirth = "2000-01-01";
+      const phone = "";
+
+      try {
+        const response = await fetch(
+          "http://localhost:8080/identity/users/register",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              username,
+              password: user.password,
+              email: user.email,
+              phone,
+              firstName,
+              lastName,
+              dayOfBirth,
+            }),
+          }
+        );
+
+        const data = await response.json();
+        if (data.code === 1000) {
+          console.log("Register successful:", data.result);
+          window.location.href = "/";
+        }
+        if (data.code === 1002) {
+          console.log("Register fail:", data.result.message);
+          setErrorMessage("Tài khoản đã tồn tại");
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        setErrorMessage("Vui lòng thử lại sau.");
+      }
+    }
+  };
+
+  const [passwordShown, setPasswordShown] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordShown(!passwordShown);
+  };
+
   return (
-    <div className='wrapper'>
-    <form onSubmit={handleSubmit}>
+    <div className="wrapper">
+      <form onSubmit={handleSubmit}>
         <h1>Đăng kí</h1>
-        <div className='form-group'>
-            <Input type='text' name="fullName" onChange={handleChange} status = {fullNameError ? 'error' : ''} placeholder='Họ và Tên' />
-            {fullNameError &&(
-                 <p className='error-message'>{fullNameError}</p>
-            )}
-           
+        <div className="form-group">
+          <Input
+            type="text"
+            name="fullName"
+            onChange={handleChange}
+            status={fullNameError ? "error" : ""}
+            placeholder="Họ và Tên"
+          />
+          {fullNameError && <p className="error-message">{fullNameError}</p>}
         </div>
-        <div className='form-group'>
-            <Input type='email' name="email" onChange={handleChange} status = {emailError? 'error' : ''}placeholder='Email' />
-            {emailError &&(
-                 <p className='error-message'>{emailError}</p>
-            )}
-            
+        <div className="form-group">
+          <Input
+            type="email"
+            name="email"
+            onChange={handleChange}
+            status={emailError ? "error" : ""}
+            placeholder="Email"
+          />
+          {emailError && <p className="error-message">{emailError}</p>}
         </div>
-
-        <div className='form-group'>
-            <Input type={passwordShown ? "text" : "password"} name="password" onChange={handleChange} status = {passwordError ? 'error' : ''} placeholder='Mật khẩu' />
-            <span onClick={togglePasswordVisibility} className='items-form'>
-                    {passwordShown ? <FaEyeSlash /> : <FaEye />}
-            </span>
-            {passwordError &&(
-                 <p className='error-message'>{passwordError}</p>
-            )}
+        <div className="form-group">
+          <Input
+            type={passwordShown ? "text" : "password"}
+            name="password"
+            onChange={handleChange}
+            status={passwordError ? "error" : ""}
+            placeholder="Mật khẩu"
+          />
+          <span onClick={togglePasswordVisibility} className="items-form">
+            {passwordShown ? <FaEyeSlash /> : <FaEye />}
+          </span>
+          {passwordError && <p className="error-message">{passwordError}</p>}
         </div>
         <div>
-            <Button htmlType='submit' type='primary'>
-                Đăng kí
-            </Button>
-        </div> 
-        
-        <div className='login-link' >
-            <div>Bạn đã có tài khoản chưa?
-            <Link to= "/login" className='text-login'>Đăng nhập</Link> 
-            </div>
-            
+          <Button htmlType="submit" type="primary">
+            Đăng kí
+          </Button>
         </div>
-    </form>
-</div>
+        <div className="login-link">
+          <div>
+            Bạn đã có tài khoản chưa?
+            <Link to="/login" className="text-login">
+              Đăng nhập
+            </Link>
+          </div>
+        </div>
+        {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+      </form>
+    </div>
   );
-}
-export default Register
+};
 
-//   return (
-    
-
-//     <div>
-//       <div>
-//         {/* login */}
-//         <form onSubmit={handleSubmitLogin}>
-//           <div>
-//             <label>Username:</label>
-//             <input
-//               type="text"
-//               value={username}
-//               onChange={(e) => setUsername(e.target.value)}
-//             />
-//           </div>
-//           <div>
-//             <label>Password:</label>
-//             <input
-//               type="password"
-//               value={password}
-//               onChange={(e) => setPassword(e.target.value)}
-//             />
-//           </div>
-//           <button type="submit">Login</button>
-//         </form>
-//         {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-
-//         {/* register */}
-//         <form onSubmit={handleSubmitRegister}>
-//           <div>
-//             <label>Username:</label>
-//             <input
-//               type="text"
-//               value={username}
-//               onChange={(e) => setUsername(e.target.value)}
-//             />
-//           </div>
-//           <div>
-//             <label>Password:</label>
-//             <input
-//               type="password"
-//               value={password}
-//               onChange={(e) => setPassword(e.target.value)}
-//             />
-//           </div>
-//           <button type="submit">Register</button>
-//           {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
+export default Register;
